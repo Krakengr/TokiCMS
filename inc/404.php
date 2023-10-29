3d78988816382507cb0136c45725b059919b946f
Akis Barekas <thunderlake@gmail.com>
2023-10-29 16:18:45 +0200

    Create 404.php

---
+++ inc/404.php
@@ -0,0 +1,12 @@
+<?php defined('TOKICMS') || die('Hacking attempt...'); ?><!DOCTYPE html>
+<html lang="<?php echo Theme::Locale() ?>">
+	<head>
+		<?php include( TOOLS_THEME_PHP_ROOT . 'head.php' ) ?>
+	</head>
+	<body>
+        <div class="main-container">
+			<?php include( TOOLS_THEME_PHP_ROOT . '404.php' ) ?>
+        </div>
+        <?php include( TOOLS_THEME_PHP_ROOT . 'footer.php' ) ?>
+    </body>
+</html>
